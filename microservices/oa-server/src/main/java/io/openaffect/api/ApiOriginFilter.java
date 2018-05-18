f4230c456fc67e77370e562423af915929b24d34
edri <mig.santamar@gmail.com>
2018-05-18 14:52:34 +0200

    CORS

--- microservices/oa-server/src/main/java/io/openaffect/api/ApiOriginFilter.java
+++ microservices/oa-server/src/main/java/io/openaffect/api/ApiOriginFilter.java
@@ -1,19 +1,26 @@
 package io.openaffect.api;
 
+import org.springframework.stereotype.Component;
+
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2017-03-05T13:15:18.047+01:00")
 
+@Component
 public class ApiOriginFilter implements Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
 		HttpServletResponse res = (HttpServletResponse) response;
-		res.addHeader("Access-Control-Allow-Origin", "*");
+
+		res.addHeader("Access-Control-Allow-Origin", req.getHeader("Origin"));
 		res.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
-		res.addHeader("Access-Control-Allow-Headers", "Content-Type");
+		res.addHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type, Accept");
+
 		chain.doFilter(request, response);
 	}
 
