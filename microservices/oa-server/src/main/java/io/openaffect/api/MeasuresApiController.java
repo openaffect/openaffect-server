247d48075bb9657ef1d705a791136d993d4d8670
Guillaume Serneels <guillaume.serneels@heig-vd.ch>
2018-05-25 17:31:16 +0200

    added new endpoint to get a list of measures for a specified triggerHref

--- microservices/oa-server/src/main/java/io/openaffect/api/MeasuresApiController.java
+++ microservices/oa-server/src/main/java/io/openaffect/api/MeasuresApiController.java
@@ -4,10 +4,13 @@ import io.openaffect.api.model.Measure;
 import io.openaffect.repositories.MeasureRepository;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -19,13 +22,30 @@ public class MeasuresApiController implements MeasuresApi {
     @Autowired
     MeasureRepository measureRepository;
 
+    private ApiError notFoundError = new ApiError(
+            HttpStatus.NOT_FOUND,
+            "A needed resource could not be found but may be available in the future.",
+            ""
+    );
+
     @Override
-    public ResponseEntity<List<Measure>> listMeasures() {
-        ResponseEntity<List<Measure>> response = new ResponseEntity<List<Measure>>(measureRepository.findAll(), HttpStatus.OK);
+    public ResponseEntity<List<Measure>> listMeasures(@ApiParam(value = "the trigger href field to look for") @RequestParam(value = "triggerHref", required = false) String triggerHref) {
+
+        if (triggerHref == null) {
+            ResponseEntity<List<Measure>> response = new ResponseEntity<List<Measure>>(measureRepository.findAll(), HttpStatus.OK);
+            return response;
+        }
+
+        List<Measure> measures = measureRepository.listByTriggerHref(triggerHref);
+        if (measures.isEmpty()){
+            notFoundError.setErrors(new StringBuilder().append("Measures with trigger href '").append(triggerHref).append("' cannot be found.").toString());
+            return new ResponseEntity(notFoundError, new HttpHeaders(), notFoundError.getStatus());
+        }
+        ResponseEntity<List<Measure>> response = new ResponseEntity<List<Measure>>(measures, HttpStatus.OK);
         return response;
     }
 
-    public ResponseEntity<Object> reportMeasure(@ApiParam(value = "measure reported by the sensor", required=true) @RequestBody Measure measure) {
+    public ResponseEntity<Object> reportMeasure(@ApiParam(value = "measure reported by the sensor", required = true) @RequestBody Measure measure) {
         // do some magic!
         measureRepository.save(measure);
         return new ResponseEntity<Object>(HttpStatus.CREATED);
