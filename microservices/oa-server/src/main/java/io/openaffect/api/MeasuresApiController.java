863f81456b89ccef547d7d9e76f1315b2c648c84
edri <mig.santamar@gmail.com>
2018-06-26 01:13:16 +0200

    Add properties attribute in emotion and add a way to get reactions by dates.

--- microservices/oa-server/src/main/java/io/openaffect/api/MeasuresApiController.java
+++ microservices/oa-server/src/main/java/io/openaffect/api/MeasuresApiController.java
@@ -3,7 +3,9 @@ package io.openaffect.api;
 import io.openaffect.api.model.Measure;
 import io.openaffect.repositories.MeasureRepository;
 import io.swagger.annotations.ApiParam;
+import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+import java.util.Date;
 import java.util.List;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2017-03-05T13:15:18.047+01:00")
@@ -49,21 +52,18 @@ public class MeasuresApiController implements MeasuresApi {
     }
 
     @Override
-    public ResponseEntity<List<Measure>> listMeasures(@ApiParam(value = "the trigger href field to look for") @RequestParam(value = "triggerHref", required = false) String triggerHref) {
+    public ResponseEntity<List<Measure>> listMeasures( @ApiParam(value = "the trigger href field to look for") @RequestParam(value = "triggerHref", required = false) String triggerHref,
+                                                       @ApiParam(value = "the inclusive date from which the measures are collected") @RequestParam(value = "fromDate", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ") DateTime fromDate,
+                                                       @ApiParam(value = "the inclusive date to which the measures are collected") @RequestParam(value = "toDate", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ") DateTime toDate) {
         if (triggerHref == null) {
-            ResponseEntity<List<Measure>> response = new ResponseEntity<List<Measure>>(measureRepository.findAll(), HttpStatus.OK);
-            return response;
+            return new ResponseEntity<List<Measure>>(measureRepository.findAll(), HttpStatus.OK);
+        } else {
+            if (fromDate == null || toDate == null) {
+                return new ResponseEntity<List<Measure>>(measureRepository.findByTriggerHref(triggerHref), HttpStatus.OK);
+            } else {
+                return new ResponseEntity<List<Measure>>(measureRepository.findByTriggerHrefAndBetweenDates(triggerHref, fromDate, toDate), HttpStatus.OK);
+            }
         }
-
-        List<Measure> measures = measureRepository.listByTriggerHref(triggerHref);
-
-        if (measures.isEmpty()){
-            notFoundError.setErrors(new StringBuilder().append("Measures with trigger href '").append(triggerHref).append("' cannot be found.").toString());
-            return new ResponseEntity(notFoundError, new HttpHeaders(), notFoundError.getStatus());
-        }
-
-        ResponseEntity<List<Measure>> response = new ResponseEntity<List<Measure>>(measures, HttpStatus.OK);
-        return response;
     }
 
     @Override
--- microservices/oa-server/src/main/java/io/openaffect/repositories/MeasureRepository.java
+++ microservices/oa-server/src/main/java/io/openaffect/repositories/MeasureRepository.java
@@ -1,9 +1,11 @@
 package io.openaffect.repositories;
 
 import io.openaffect.api.model.Measure;
+import org.joda.time.DateTime;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -12,7 +14,13 @@ import java.util.List;
 public interface MeasureRepository extends MongoRepository<Measure, String> {
     // Find by trigger href, if the href contains the given string.
     @Query("{trigger.href: {$regex: '.*?0.*'}}")
-    List<Measure> listByTriggerHref(String href);
+    List<Measure> findByTriggerHref(String href);
+
+    @Query("{timestamp: {$gte: ?1, $lte: ?2}}")
+    List<Measure> findBetweenDates(DateTime fromDate, DateTime toDate);
+
+    @Query("{trigger.href: {$regex: '.*?0.*'}, timestamp: {$gte: ?1, $lte: ?2}}")
+    List<Measure> findByTriggerHrefAndBetweenDates(String href, DateTime fromDate, DateTime toDate);
 
     @Query(value = "{trigger.href: ?0}", delete = true)
     void deleteByTriggerHref(String href);
